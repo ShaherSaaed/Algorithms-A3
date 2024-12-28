9b3116c197d69ab6bf93ecbfcf83d34e3e2b9f0c
ShaherSaaed <shahersa3ed004@gmail.com>
2024-12-28 09:52:48 +0200

    Done working on problem1

---
+++ Problem1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int findMinDiff(vector<int>& coins) {
+    int totalSum = 0;
+    for (int coin : coins) {
+        totalSum += coin;
+    }
+
+    // dp[i][j] represents if sum j is possible with first i coins
+    vector<vector<bool>> dp(coins.size() + 1, vector<bool>(totalSum + 1, false));
+
+    // Empty subset has sum 0
+    for (int i = 0; i <= coins.size(); i++) {
+        dp[i][0] = true;
+    }
+
+    // Fill dp table
+    for (int i = 1; i <= coins.size(); i++) {
+        for (int j = 1; j <= totalSum; j++) {
+            dp[i][j] = dp[i-1][j]; // Don't take current coin
+            if (j >= coins[i-1]) {
+                dp[i][j] = dp[i][j] || dp[i-1][j-coins[i-1]]; // Take current coin
+            }
+        }
+    }
+
+    // Find minimum difference
+    int minDiff = totalSum;
+    for (int j = 0; j <= totalSum/2; j++) {
+        if (dp[coins.size()][j]) {
+            minDiff = min(minDiff, totalSum - 2*j);
+        }
+    }
+
+    return minDiff;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int m;
+        cin >> m;
+
+        vector<int> coins(m);
+        for (int i = 0; i < m; i++) {
+            cin >> coins[i];
+        }
+
+        cout << findMinDiff(coins) << endl;
+    }
+
+    return 0;
+}
